8709774345869615cfa45e0fcab965fb1d4896dd
mac <mac@macs-MacBook-Pro.local>
2023-10-08 01:15:23 -0700

    First Commit

---
+++ ChineseZodiac.py
@@ -0,0 +1,86 @@
+year=input("\nWelcome to Chinese Zodiac. Enter your year of birth in YYYY format:")
+
+rat=[1948, 1960, 1972, 1984, 1996, 2008, 2020]
+ox=[1949, 1961, 1973, 1985, 1997, 2009, 2021]
+tiger=[1950, 1962, 1974, 1986, 1998, 2010, 2022]
+rabbit=[1951, 1963, 1975, 1987, 1999, 2011, 2023]
+dragon=[1952, 1964, 1976, 1988, 2000, 2012, 2024]
+snake=[1953, 1965, 1977, 1989, 2001, 2013, 2025]
+horse=[1954, 1966, 1978, 1990, 2002, 2014, 2026] 
+goat=[1955, 1967, 1979, 1991, 2003, 2015, 2027]
+monkey=[1956, 1968, 1980, 1992, 2004, 2016, 2028]
+rooster=[1957, 1969, 1981, 1993, 2005, 2017, 2029]
+dog=[1958, 1970, 1982, 1994, 2006, 2018, 2030]
+pig=[1959, 1971, 1983, 1995, 2007, 2019, 2031]
+
+for item in rat:
+    if str(item) == year:
+        print("\nRAT: Rats are quick-witted, resourceful, and smart but lack courage. With rich imaginations and sharp observations, they can take advantage of various opportunities well.")
+        print("\nRead More: https://www.chinahighlights.com/travelguide/chinese-zodiac/rat.htm\n")
+        break
+
+for item in ox:
+    if str(item) == year:
+        print("\nOX: The zodiac sign Ox occupies the second position in the Chinese Zodiac. As the strongest animal of the 12 Chinese zodiac signs, known as 'the good helper' in Chinese farming, the Ox is a symbol of diligence, strength, honesty, down-to-earth persistence, and wealth.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/ox.htm\n")
+        break
+
+for item in tiger:
+    if str(item) == year:
+        print("\nTIGER: The Tiger is known as the king of all beasts in China. The zodiac sign Tiger is a symbol of strength, exorcising evils, and braveness. Many Chinese kids wear hats or shoes with a tiger image for good luck. Tiger people, you have a down-to-earth personality and down-to-earth work ethic. With great confidence and indomitable fortitude, they can be competent leaders.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/tiger.htm\n")
+        break
+
+for item in rabbit:
+    if str(item) == year:
+        print("\nRABBIT: The sign of Rabbit is a symbol of longevity, peace, and prosperity in Chinese culture. 2023 is predicted to be a year of hope. People born in a year of the Rabbit are called 'Rabbits' and are believed to be vigilant, witty, quick-minded, and ingenious.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/rabbit.htm\n")
+        break
+    
+for item in dragon:
+    if str(item) == year:
+        print("\nDRAGON: The Dragon symbolizes power, nobleness, honor, luck, and success in traditional Chinese culture. The Dragon is a supernatural being with no parallel for talent and excellence.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/dragon.htm\n")
+        break
+
+for item in snake:
+    if str(item) == year:
+        print("\nSNAKE: The Snake is one of the three luckiest zodiac signs in 2023. Snakes will develop smoothly in a good direction. According to Chinese astrology, people born in a year of the Snake are usually calm observers with excellent intuition, as serene as snakes, and generally demonstrate intelligence, wisdom, and creativity.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/snake.htm\n")
+        break
+
+for item in horse:
+    if str(item) == year:
+        print("\nHORSE: In Chinese traditional culture, horses were domesticated for transport and fighting battles. The strengths and features they symbolize are freedom, speed, boldness, and loyalty. The term 'swift horses' means 'talented people' and 'geniuses' in traditional Chinese culture. People born in a year of the Horse usually have unerring instincts and keen powers of observation. They are enthusiastic and gregarious, though they certainly do not lack independent thinking.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/horse.htm\n")
+        break
+
+for item in goat:
+    if str(item) == year:
+        print("\nGOAT: People born in a year of the Goat are generally believed to be gentle, mild-mannered, shy, stable, sympathetic, amicable, and brimming with a strong sense of kindheartedness and justice. They have very delicate thoughts, strong creativity, and perseverance, and acquire professional skills well. Although they look gentle on the surface, they are tough on the inside, always insisting on their own opinions in their minds. They have strong inner resilience and excellent defensive instincts.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/goat.htm\n")
+        break
+
+for item in monkey:
+    if str(item) == year:
+        print("\nMONKEY: People born in a year of the Monkey have magnetic personalities and are witty and intelligent. Personality traits like mischievousness, curiosity, and cleverness make them very naughty. Monkeys are masters of practical jokes, because they like playing most of the time. Although they don't have bad intentions, their pranks sometimes hurt other people's feelings. Monkeys are fast learners and crafty opportunists. They have many interests and need partners who are capable of stimulating them. While some like the eccentric nature of Monkeys, others don't trust their sly, restless, and inquisitive nature. Although they are clever and creative, Monkeys can't always exhibit their talents properly. They like to accept challenges and prefer urban life to rural life.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/monkey.htm\n")
+        break
+
+for item in rooster:
+    if str(item) == year:
+        print("\nROOSTER: Roosters are always active, amusing, and popular within a crowd. Roosters are talkative, outspoken, frank, open, honest, and loyal individuals. They like to be the center of attention and always appear attractive and beautiful. Roosters are happiest when they are surrounded by others, whether at a party or just a social gathering. They enjoy the spotlight and will exhibit their charm on any occasion. Roosters expect others to listen to them while they speak, and can become agitated if they don't. Vain and boastful, Roosters like to brag about themselves and their accomplishments.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/rooster.htm\n")
+        break
+
+for item in dog:
+    if str(item) == year:
+        print("\nDOG: Dogs are loyal and honest, amiable and kind, cautious and prudent. Due to having a strong sense of loyalty and sincerity, Dogs will do everything for the person who they think is most important. As Dogs are not good at communication, it is difficult for them to convey their thoughts to others. Therefore, Dogs tend to leave others with the impression that they have a stubborn personality. When thrown into doubt, Dogs think the world is evil and complicated. Then they criticize sharply when making comments, and infer that all things are according to their pessimistic point of view.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/dog.htm\n")
+        break
+
+for item in pig:
+    if str(item) == year:
+        print("\nPIG: With a round and fat face, the Pig is the symbol of wealth, felicity, honesty, and practicality in Chinese zodiac culture. Many money pots (Chinese piggybanks) are made pig-shaped as people believe Pig invocation will bring good luck monetarily. Pigs are diligent, compassionate, and generous. They have great concentration: once they set a goal, they will devote all their energy to achieving it. Though Pigs rarely seek help from others, they will not refuse to give others a hand. Pigs never suspect trickery, so they are easily fooled. Generally speaking, Pigs are relatively calm when facing trouble. No matter how difficult the problems are Pigs encounter, they can handle things properly and carefully. They have a great sense of responsibility to finish what they are engaged in.")
+        print("\nRead More:https://www.chinahighlights.com/travelguide/chinese-zodiac/pig.htm\n")
+        break
